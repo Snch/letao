81a9427ce5e420d9d42fdf5dc1a46dc8b39d6ff6
ch <973719669@qq.com>
2018-09-26 23:49:55 +0800

    登陆-ajax请求功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -24,6 +24,9 @@ $(function () {
             min: 2,
             max: 6,
             message: "用户名长度在2~6位"
+          },
+          callback : {
+            message: "用户名错误"
           }
         }
       },
@@ -36,6 +39,9 @@ $(function () {
             min: 6,
             max: 12,
             message: "密码长度为6~12位"
+          },
+          callback: {
+            message: "密码错误"
           }
         }
       }
@@ -43,7 +49,37 @@ $(function () {
 
   });
 
+  // 
+  $("#form").on("success.form.bv",function (e) {  
+    // 阻止默认表单提交
+    e.preventDefault();
+    // ajax 提交
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      // 表单序列化 快速获取
+      data: $("#form").serialize(),
+      dataType: "json",
+      success: function (info) {  
+        // console.log(info);
+        if(info.success) {
+          location.href = "index.html";
+        }
+        if(info.error === 1000) {
+          // alert("用户名不存在")
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback")
+        }
+        if(info.error === 1001) {
+          // alert("密码错误")
+          $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback")
+        }
+      }
+    });
+  });
 
-
+  // 表单重置
+  $('[type="reset"]').click(function () {  
+    $("#form").data("bootstrapValidator").resetForm();
+  })
 
 });
