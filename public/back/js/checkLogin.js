55f97bda61c6454840fb354566c55bf1f20b2903
ch <973719669@qq.com>
2018-09-29 19:47:53 +0800

    公共样式完成-登陆退出-拦截完成

---
+++ public/back/js/checkLogin.js
@@ -0,0 +1,12 @@
+// 4 登录拦截功能
+$.ajax({
+  type: "get",
+  url: "/employee/checkRootLogin",
+  dataType: "json",
+  success: function (info) {  
+
+    if(info.error === 400 ) {
+      location.href = "login.html"
+    }
+  }
+})
--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,7 +2,42 @@
 //  需求: 发送第一个ajax时开启进度条 返回全部时结束
 $(document).ajaxStart(function () {  
   NProgress.start();
-})
+});
 $(document).ajaxStop(function () {  
   NProgress.done();
-})
+});
+
+// 公共效果
+
+$(function () {  
+
+// 1 二级菜单切换效果
+  $(".nav .category").on("click",function () {  
+    $(".child").stop().slideToggle();
+  });
+
+// 2 左侧菜单切换
+  $(".lt_main .pull-left").on("click",function () {  
+    $(".lt_aside").toggleClass("hidemenu");
+    $(".lt_topbar").toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+  });
+
+
+// 3 退出功能
+  $(".lt_main .pull-right").on("click",function () {  
+    $("#logoutModal").modal("show");
+  });
+  $("#logoutBtn").click(function () {  
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogin',
+      dataType: "json",
+      success: function (info) {  
+
+      }
+    });
+  });
+
+
+});
